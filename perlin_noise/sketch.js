aa68c7a4fd837542d84b565eb280145519eb7291
Charlotte Roe <charlotte.kj.roe@gmail.com>
2023-02-06 21:32:22 +0000

    circle moving along x axis according to perlin noise values

--- perlin_noise/sketch.js
+++ perlin_noise/sketch.js
@@ -1,7 +1,15 @@
+let xoff = 0;
+
 function setup() {
   createCanvas(400, 400);
 }
 
 function draw() {
   background(220);
+  //let x = random(width);
+  let x = map(noise(xoff), 0, 1, 0, width);
+
+  xoff += 0.01;
+
+  ellipse(x, 200, 25, 25);
 }
