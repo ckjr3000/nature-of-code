65a12a944e0f440c76e1c01fdeae41d80734457d
Charlotte Roe <charlotte.kj.roe@gmail.com>
2023-02-07 20:18:17 +0000

    moving around the whole canvas

--- perlin_noise/sketch.js
+++ perlin_noise/sketch.js
@@ -1,15 +1,19 @@
-let xoff = 0;
+let xoff1 = 0;
+let xoff2 = 10000;
 
 function setup() {
   createCanvas(400, 400);
+  //background(220);
 }
 
 function draw() {
   background(220);
-  //let x = random(width);
-  let x = map(noise(xoff), 0, 1, 0, width);
 
-  xoff += 0.01;
+  let x = map(noise(xoff1), 0, 1, 0, width);
+  let y = map(noise(xoff2), 0, 1, 0, height);
 
-  ellipse(x, 200, 25, 25);
+  xoff1 += 0.02;
+  xoff2 += 0.01;
+
+  ellipse(x, y, 25, 25);
 }
