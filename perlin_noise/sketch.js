355d8acce0d5ab7f93e2fe9902724dddd33654df
Charlotte Roe <charlotte.kj.roe@gmail.com>
2023-02-07 21:21:26 +0000

    animated 2D perlin noise

--- perlin_noise/sketch.js
+++ perlin_noise/sketch.js
@@ -1,14 +1,16 @@
 let inc = 0.02;
+let start = 0;
 
 function setup() {
   createCanvas(200, 200);
+  noiseDetail(16);
   //background(220);
 }
 
 function draw() {
   loadPixels();
   background(220);
-  let yoff = 0;
+  let yoff = start;
 
   for (let y = 0; y < height; y++) {
     let xoff = 0;
@@ -25,5 +27,5 @@ function draw() {
     yoff += inc;
   }
   updatePixels();
-  //noLoop();
+  start += inc;
 }
