1a8c373b1db6fc57eb94f9b2ce081a2b06d6a189
Charlotte Roe <charlotte.kj.roe@gmail.com>
2023-02-07 21:08:51 +0000

    2d perlin noise

--- perlin_noise/sketch.js
+++ perlin_noise/sketch.js
@@ -1,24 +1,29 @@
 let inc = 0.02;
-let start = 0;
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(200, 200);
   //background(220);
 }
 
 function draw() {
+  loadPixels();
   background(220);
+  let yoff = 0;
 
-  stroke(0);
-  noFill();
-  beginShape();
-  let xoff = start;
-  for (let x = 0; x < width; x++) {
-    y = noise(xoff) * height;
-    vertex(x, y);
-    xoff += inc;
+  for (let y = 0; y < height; y++) {
+    let xoff = 0;
+    for (let x = 0; x < width; x++) {
+      let index = (x + y * width) * 4;
+      //let r = random(255);
+      let n = noise(xoff, yoff) * 255;
+      pixels[index + 0] = n;
+      pixels[index + 1] = n;
+      pixels[index + 2] = n;
+      pixels[index + 3] = 255;
+      xoff += inc;
+    }
+    yoff += inc;
   }
-  endShape();
-  start += inc;
+  updatePixels();
   //noLoop();
 }
